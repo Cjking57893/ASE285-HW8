91fb3f9ad23f8e3167444bfb9f92064b4f8a1b8f
Cody King <122841869+Cjking57893@users.noreply.github.com>
2024-05-03 17:48:33 -0400

    passwordjs.js complete

--- src/passwordjs.js
+++ src/passwordjs.js
@@ -1,6 +1,6 @@
 'use strict'
 const fs = require('fs');
-const util = require('./utility')
+const {readFile, writeFile, hash} = require('./utility');
 
 function passwordjs() {
     if (process.argv.length != 5) return 'false';
@@ -8,12 +8,19 @@ function passwordjs() {
     var filename = process.argv[2];
     var email = process.argv[3];
     var password = process.argv[4];
-
     
+    const file = readFile(filename);
+    
+    if (file.includes(email + ':' + hash(password))) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 if (require.main === module) {
     console.log(passwordjs()) // print out true or false
 }
 
-module.exports = {???};
+module.exports = {passwordjs};
